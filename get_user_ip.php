5a8f184b55b231a575b6782ab5ae83e3b0b166bf
MaxStu05 <111632202+MaxStu05@users.noreply.github.com>
2023-02-10 22:44:20 +0100

    Delete get_user_ip.php

--- get_user_ip.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Content-Type: application/json');
-$ips = json_decode(file_get_contents('ips.json'), true);
-$ips['ips'][] = $_SERVER['REMOTE_ADDR'];
-file_put_contents('ips.json', json_encode($ips));
-echo json_encode(array('ip' => $_SERVER['REMOTE_ADDR']));
